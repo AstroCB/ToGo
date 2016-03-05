0404c22fb91a3b234f8d48f9029e81f094ab9697
Cameron Bernhardt <cambernhardt@me.com>
2016-03-05 14:29:58 -0500

    Basic setup complete

--- TestingAPIS/map.js
+++ TestingAPIS/map.js
@@ -1,32 +1,53 @@
+var map, lat, long, homeMarker;
+
 function initMap() {
-    createMap()
     getLocation();
 }
 
 function getLocation() {
     if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(locatePosition);
+        setTimeout(function() {
+          createMarker(map.center.lat(), map.center.lng());
+        }, 1500);
+    } else {
+        createMap();
     }
 }
 
 function locatePosition(position) {
-  createMap(position.coords.latitude, position.coords.longitude);
+    createMap(position.coords.latitude, position.coords.longitude);
 }
 
 function createMap(lat, long) {
-  if(!lat) {
-    lat = 39.290385;
-  }
-  if(!long) {
-    long = -76.612189;
-  }
-  var map = new google.maps.Map(document.getElementById('map'), {
-      center: {
-          lat: lat,
-          lng: long
-      },
-      scrollwheel: true,
-      zoom: 15
-  });
-  return map;
+    if (!lat) {
+        latitude = 39.290385;
+    } else {
+        latitude = lat;
+    }
+    if (!long) {
+        longitude = -76.612189;
+    } else {
+        longitude = long;
+    }
+
+    var newMap = new google.maps.Map(document.getElementById('map'), {
+        center: {
+            lat: latitude,
+            lng: longitude
+        },
+        scrollwheel: true,
+        zoom: 15
+    });
+    map = newMap;
+}
+
+function createMarker(lat, long) {
+    homeMarker = new google.maps.Marker({
+        position: {
+            lat: lat,
+            lng: long
+        },
+        map: map
+    });
 }
