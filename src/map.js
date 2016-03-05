9f9a71af52d0df9204cffdfee8d6c02888d58fda
Cameron Bernhardt <cambernhardt@me.com>
2016-03-05 15:18:20 -0500

    Added map

---
+++ src/map.js
@@ -0,0 +1,52 @@
+var map, lat, long, homeMarker;
+
+function initMap() {
+    getLocation();
+}
+
+function getLocation() {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(locatePosition);
+    } else {
+        createMap();
+    }
+}
+
+function locatePosition(position) {
+    createMap(position.coords.latitude, position.coords.longitude);
+}
+
+function createMap(lat, long) {
+    if (!lat) {
+        latitude = 39.290385;
+    } else {
+        latitude = lat;
+    }
+    if (!long) {
+        longitude = -76.612189;
+    } else {
+        longitude = long;
+    }
+
+    var newMap = new google.maps.Map(document.getElementById('map'), {
+        center: {
+            lat: latitude,
+            lng: longitude
+        },
+        scrollwheel: true,
+        zoom: 15
+    });
+    map = newMap;
+    homeMarker = createMarker(map.center.lat(), map.center.lng());
+}
+
+function createMarker(lat, long) {
+    var mark = new google.maps.Marker({
+        position: {
+            lat: lat,
+            lng: long
+        },
+        map: map
+    });
+    return mark;
+}
