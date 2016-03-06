2dcf1a3a80e11ff5144c49ff4cab5c713501abcc
Cameron Bernhardt <cambernhardt@me.com>
2016-03-05 22:24:26 -0500

    Weather API created

--- TestingAPIS/map.js
+++ TestingAPIS/map.js
@@ -1,12 +1,18 @@
-var map, lat, long, homeMarker;
+var map, lat, long, homeMarker, finalMarker, directionsDisplay;
 
 function initMap() {
     getLocation();
+    $("#datepicker").datepicker({
+        minDate: 0,
+        dateFormat: "mm/dd/yy"
+    }).datepicker("setDate", new Date());
 }
 
 function getLocation() {
     if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(locatePosition);
+        navigator.geolocation.getCurrentPosition(locatePosition, function() {
+            createMap();
+        });
     } else {
         createMap();
     }
@@ -37,16 +43,119 @@ function createMap(lat, long) {
         zoom: 15
     });
     map = newMap;
-    homeMarker = createMarker(map.center.lat(), map.center.lng());
+
+    initializeServices();
 }
 
-function createMarker(lat, long) {
+function initializeServices() {
+    homeMarker = createMarker(map.center.lat(), map.center.lng(), "from.png");
+    document.getElementById("from").value = map.center.lat() + ", " + map.center.lng();
+
+    var randLat = map.center.lat() + randomSmallValue(),
+        randLong = map.center.lng() + randomSmallValue();
+    finalMarker = createMarker(randLat, randLong, "to.png");
+    document.getElementById("to").value = randLat + ", " + randLong;
+
+    homeMarker.addListener("dragend", homeDragEnded);
+    finalMarker.addListener("dragend", finalDragEnded);
+
+    directionsDisplay = new google.maps.DirectionsRenderer();
+    directionsDisplay.setMap(map);
+    directionsDisplay.setPanel(document.getElementById("directionsPanel"));
+}
+
+function createMarker(lat, long, image) {
     var mark = new google.maps.Marker({
         position: {
             lat: lat,
             lng: long
         },
-        map: map
+        map: map,
+        draggable: true,
+        animation: google.maps.Animation.DROP,
+        icon: "images/" + image
     });
     return mark;
+}
+
+function randomSmallValue() {
+    var rand = Math.random() * 0.006 + 0.003,
+        decider = Math.random();
+    if (decider <= 0.5) {
+        rand *= -1;
+    }
+    return rand;
+}
+
+function homeDragEnded(e) {
+    document.getElementById("from").value = e.latLng.lat() + ", " + e.latLng.lng();
+}
+
+function finalDragEnded(e) {
+    document.getElementById("to").value = e.latLng.lat() + ", " + e.latLng.lng();
+}
+
+function getDirections() {
+    var directionsService = new google.maps.DirectionsService();
+    var req = {
+        origin: homeMarker.position,
+        destination: finalMarker.position,
+        provideRouteAlternatives: false,
+        travelMode: google.maps.TravelMode.DRIVING,
+        drivingOptions: {
+            departureTime: $("#datepicker").datepicker("getDate"),
+            trafficModel: google.maps.TrafficModel.PESSIMISTIC
+        },
+        unitSystem: google.maps.UnitSystem.IMPERIAL
+    };
+    directionsService.route(req, function(result, status) {
+        if (status == google.maps.DirectionsStatus.OK) {
+            directionsDisplay.setDirections(result);
+            homeMarker.setMap(null);
+            finalMarker.setMap(null);
+        }
+    });
+}
+
+function getWeather() {
+    var date = $("#datepicker").datepicker("getDate");
+    var today = new Date();
+    var dd = today.getDate();
+    var mm = today.getMonth() + 1; //January is 0!
+    var yyyy = today.getFullYear();
+
+    if (dd < 10) {
+        dd = '0' + dd
+    }
+
+    if (mm < 10) {
+        mm = '0' + mm
+    }
+
+    var url = "https://api.forecast.io/forecast/88e8ca844f0b17a64b8fd82368b332d0/" + finalMarker.position.lat() + "," + finalMarker.position.lng() + "," + yyyy + "-" + mm + "-" + dd + "T12:00:00";
+    var req = new XMLHttpRequest();
+    req.open("GET", url, true);
+    req.addEventListener("load", function() {
+        if (req.readyState == 4 && req.status == 200) {
+            var data = JSON.parse(req.responseText).daily.data[0];
+            console.log(data);
+            var vals = {
+                "Summary": data.summary,
+                "Visibility": data.visibility + " mi",
+                "Precipitation Chance": data.precipProbability * 100 + "%",
+                "Humidity": data.humidity * 100 + "%",
+                "High Temp": data.temperatureMax,
+                "Low Temp": data.temperatureMin
+            };
+
+            for(var i in vals) {
+              var innerString = i + ": " + vals[i];
+              if(i === "Summary") {
+                innerString = vals[i];
+              }
+              $("#weather").append("<span class='weatherItem'>" + innerString + "</span><br/>");
+            }
+        }
+    }, false);
+    req.send(null);
 }
