a56a8099d88fd5d0dc62f761459b7c106631181e
Cameron Bernhardt <cambernhardt@me.com>
2016-03-06 03:30:02 -0500

    Fix

--- src/map.js
+++ src/map.js
@@ -217,7 +217,7 @@ function getWeather() {
     $.getJSON(
         "https://api.forecast.io/forecast/88e8ca844f0b17a64b8fd82368b332d0/" +
         finalMarker.position.lat() + "," + finalMarker.position.lng() +
-        "," + yyyy + "-" + mm + "-" + dd + "T12:00:00" + "?callback=?",
+        "," + yyyy + "-" + mm + "-" + dd + "T12:00:00" + "&callback=?",
         function(req) {
             var data = JSON.parse(req.responseText).daily.data[0];
             var vals = {
